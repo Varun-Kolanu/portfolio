e2f7043cd28c22721fbc270c9463d9bd1566cab0
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-04-14 04:02:18 +0530

    Home3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ export default function Navbar() {
             <img src={logo} alt="V" className='h-12 absolute top-2 left-2 cursor-pointer'/>
             <div id="navmenu" className='absolute right-60'>
                 <ul className='flex'>
-                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer'>HOME</li>
-                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer'>ABOUT</li>
-                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer'>SKILLS</li>
-                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer'>MY PROJECTS</li>
+                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer transition-all duration-500'>HOME</li>
+                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer transition-all duration-500'>ABOUT</li>
+                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer transition-all duration-500'>SKILLS</li>
+                    <li className='px-8 text-white font-bold hover:underline hover:underline-offset-4 hover:text-orange-500 cursor-pointer transition-all duration-500'>MY PROJECTS</li>
                 </ul>
             </div>
         </nav>
