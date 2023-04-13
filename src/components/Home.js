00bdeb4df3b93caeac4a17ddb364032f06b6fa45
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-04-14 04:00:05 +0530

    HomePage2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,18 @@ export default function Home() {
   return (
     <div className="h-[100vh] relative flex justify-center">
         <div id="home" className='w-[1000px] h-[92vh] self-end grid grid-flow-row grid-cols-2'>
-            <div id="intro" className='pt-44 text-white'>
+            <div id="intro" className='pt-44 text-white w-[520px]'>
                 <div id="hi" className='text-[1.5rem]'>Hi There! 👋</div>
-                <div id="name" className='text-[3.5rem] font-mono'>Myself <span className='text-red-950'>Varun</span> </div>
+                <div id="name" className='text-[3.5rem] font-mono'>Myself <span className='text-orange-400'>Varun</span> </div>
                 <div id="description" className='font-mono'>
-                    I'm a Deve-love-per, transforming ideas into high-quality and functional Web Projects on time and budget
+                    I'm a Deve-love-per, transforming ideas into high-quality and functional Web Projects, on time and in budget
                 </div>
                 <div id="button" className='flex justify-center mt-4'>
                     <Button btnText="Contact"/>
                 </div>
             </div>
-            <div id="introImgDiv" className='flex items-center justify-center'>
-                <img src={introImg} alt="Developer" id="introImg" className='rounded-[30%]'/>
+            <div id="introImgDiv" className='flex items-center justify-center relative left-6'>
+                <img src={introImg} alt="Developer" id="introImg" className='rounded-[30%] w-[120%]'/>
             </div>
         </div>
     </div>
