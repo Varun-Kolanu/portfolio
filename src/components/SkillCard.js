386322170bb187bc87d8e3020ba486ba28680417
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-04-19 12:54:49 +0530

    responsive update2

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SkillCard(props) {
   return (
     <div
-      className="skillCard w-[150px] h-[150px] my-5 mx-1 sm:w-[220px] sm:h-[220px] shadow-xl shadow-gray-500 sm:mx-3 sm:my-5 flex flex-col items-center justify-evenly bg-gradient-to-br from-blue-500 from-30% to-indigo-600 rounded-[24px] hover:scale-105 hide"
+      className="skillCard w-[150px] h-[150px] my-3 mx-1 sm:w-[220px] sm:h-[220px] shadow-xl shadow-gray-500 sm:mx-3 sm:my-5 flex flex-col items-center justify-evenly bg-gradient-to-br from-blue-500 from-30% to-indigo-600 rounded-[24px] hover:scale-105 hide"
       id={props.id}
     >
       <div className="skillLogo text-4xl sm:text-5xl flex">{props.img}</div>
