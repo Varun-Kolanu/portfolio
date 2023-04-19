88f32c0f6ce46e6b7a1c0a264e5d2ffb21dcfbc5
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-04-19 13:50:21 +0530

    project commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,12 +6,12 @@ export default function ProjectCard(props) {
   return (
     <div
       id={props.id}
-      className="projectCard lg:w-[370px] md:w-[500px] sm:w-[380px] bg-gradient-to-br from-blue-400 from-30% to-blue-600 my-4 sm:m-5 flex flex-col items-center rounded-3xl hide"
+      className="projectCard lg:w-[370px] md:w-[500px] sm:w-[400px] w-[300px] bg-gradient-to-br from-blue-400 from-30% to-blue-600 my-4 sm:m-5 flex flex-col items-center rounded-3xl hide"
     >
       <iframe
         src={props.img}
         title="Iframe"
-        className="md:w-[480px] lg:w-[350px] sm:w-[380px] h-[400px] rounded-3xl relative top-2"
+        className="md:w-[480px] lg:w-[350px] sm:w-[380px] w-[280px] h-[400px] rounded-3xl relative top-[10px]"
       ></iframe>
       <p className="text-center text-white mt-5 text-lg">{props.desc}</p>
       <div className="projectBtns flex my-5">
