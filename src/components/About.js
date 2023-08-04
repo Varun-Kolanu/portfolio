173fc3efb4458015a054e27a8c0fb126358f579a
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-08-04 19:24:27 +0530

    2nd year

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ export default function About() {
           id="aboutDescription"
           className=" leading-loose text-white lg:text-[1.1rem] md:text-[1rem] lg:w-[800px] md:w-[600px] sm:w-[500px]"
         >
-          Hi! Myself Varun Kolanu, CSE 1st year, from IIT (BHU) Varanasi.
+          Hi! Myself Varun Kolanu, CSE 2nd year, from IIT (BHU) Varanasi.
           <br />
           I like to learn new technologies, languages and cool stuff. I love to
           develop new things which help individuals practically. I have done
